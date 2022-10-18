4a494f119d5235543a1e8f305c989b91eb0e8dbc
JoseFernandes975 <zehenrique752005@gmail.com>
2022-10-18 12:34:55 -0300

    Create and Implement project hr-worker

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 150.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 100.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 300.0);
+
