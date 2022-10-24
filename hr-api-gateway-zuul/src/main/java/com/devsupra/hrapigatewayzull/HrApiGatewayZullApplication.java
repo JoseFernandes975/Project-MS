95f606cc53e89c5302dfaa4a7137e4ec25861d49
JoseFernandes975 <zehenrique752005@gmail.com>
2022-10-24 13:45:01 -0300

    Configuration Zull api gateway

--- hr-api-gateway-zuul/src/main/java/com/devsupra/hrapigatewayzull/HrApiGatewayZullApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/devsupra/hrapigatewayzull/HrApiGatewayZullApplication.java
@@ -2,7 +2,13 @@ package com.devsupra.hrapigatewayzull;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class HrApiGatewayZullApplication {
 
