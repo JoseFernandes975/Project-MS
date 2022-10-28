f7fcefe1cd79bc32e4767d5f4d78638f85a06ff3
JoseFernandes975 <zehenrique752005@gmail.com>
2022-10-27 21:15:09 -0300

    Create project hr-user

--- hr-config-server/src/main/java/com/devsupra/hrapigatewayzull/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsupra/hrapigatewayzull/HrConfigServerApplication.java
@@ -24,8 +24,8 @@ public class HrConfigServerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-	//	System.out.println("USERNAME = " + username);
-	//	System.out.println("PASSWORD = " + password);
+		System.out.println("USERNAME = " + username);
+		System.out.println("PASSWORD = " + password);
 	}
 
 }
