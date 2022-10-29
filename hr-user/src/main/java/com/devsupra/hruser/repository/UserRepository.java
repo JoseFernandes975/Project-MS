56f5ea873ef8c2835755fe575f025cd4c540412e
JoseFernandes975 <zehenrique752005@gmail.com>
2022-10-28 22:07:49 -0300

    Configuration hr-oauth a conection eureka

--- hr-user/src/main/java/com/devsupra/hruser/repository/UserRepository.java
+++ hr-user/src/main/java/com/devsupra/hruser/repository/UserRepository.java
@@ -1,14 +1,9 @@
 package com.devsupra.hruser.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.devsupra.hruser.entities.User;
-
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	User findByEmail(String email);
-	 
 }
